68c3616def23430e93fcc0711e026b7299a395c9
Ayush Thakur <tsayush2390@gmail.com>
2024-11-18 11:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def download_video():
         download_path = get_download_folder()
 
         # Initialize YouTube object and get the highest resolution stream
-        yt = YouTube(video_url, on_progress_callback=on_progress)
+        yt = YouTube(video_url, on_progress_callback=on_progress, use_po_token=True, po_token_verifier={"po_token": po_token})
         # ys = yt.streams.get_highest_resolution()
 
         if method == 'itag_18':
